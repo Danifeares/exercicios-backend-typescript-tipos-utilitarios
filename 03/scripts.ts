bc44ebcaa2ab9dc59d1c33b3636a05515a5d3cda
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-11-10 16:50:57 -0300

    Resolução do exercício 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,24 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+// criado um tipo para o retorno da função conexão, para então poder manipular esse retorno usando ReadOnly!
+type Conexao = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<Conexao> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+const conecta = conexao({
+    username: 'dani',
+    password: '123',
+    host: 'localhost',
+    port: '5432',
+    dbname: 'pg'
+})
