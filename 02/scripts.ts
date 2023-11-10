4687c602db7f014c33fbbbe7e1940b9bdac30603
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-11-10 16:44:11 -0300

    Resolução do exercício 2

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,31 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string, 
+    estado: string
+}
+
+// type CarrinhoComEndereco = {
+//     item: Item,
+//     qtd: number,
+//     desconto: number,
+//     frete: number,
+//     tipoTransacao: Lowercase<'credito' | 'Debito'>, // aqui ele só vai aceitar caracteres minúsculos
+//     cartao: Cartao,
+//     endereco: Endereco
+// }
+
+// pode-se fazer dessa outra forma tbm: 
+
+// Omito o tipoTransacao para setar ele novamente depois, com o operador "&", adiciono o tipo Carrinho, 
+// e  endereco e tipoTransacao ao tipo CarrinhoComEndereco:
+
+type CarrinhoComEndereco = Omit<Carrinho, 'tipoTransacao'> & {
+    endereco: Endereco,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>
+}
